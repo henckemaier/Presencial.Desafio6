58747e02435339be85789b0a7659efcdba0a07a0
ALUNO <ALUNO@DESKTOP-LNRR01U>
2022-02-18 16:27:27 -0300

    Add project files.

---
+++ Presencial.Desafio6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Presencial.Desafio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o numero de homens: ");
+            string contHomems = Console.ReadLine();
+            int homensCont = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Insira o numero de saltos: ");
+            string saltoHomems = Console.ReadLine();
+            int homemSalto = Convert.ToInt32(saltoHomems);
+
+            for(int i = 1; i > homensCont ; i++)
+            {
+                if()
+                {
+
+                }
+            }
+
+        }
+    }
+}
